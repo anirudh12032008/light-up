81ebc74c0da6dbc49a4012b5de216fb5d2deadf9
Anirudh Sahu <renudilip19@gmail.com>
2024-10-16 00:01:50 +0530

    changed a bit of UI

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 Shery.mouseFollower({
   skew: true,
   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
+  duration: 0.2,
 });
 
 
@@ -9,21 +9,20 @@ Shery.mouseFollower({
 Shery.imageEffect("#back", {
   style: 5,
   gooey: true,
-  config: {"a":{"value":2,"range":[0,30]},"b":{"value":-0.71,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.0850077279752703},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.02,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.15,"range":[0,2]},"discard_threshold":{"value":0.4,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.45,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+  config: {"a":{"value":2,"range":[0,30]},"b":{"value":0.95,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.0850077731382726},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.02,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.15,"range":[0,2]},"discard_threshold":{"value":0.4,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.45,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
 });
 
 
+yg uhjkmjhv  uihj yucj jhgj hujipoik,klmkp
 document.querySelectorAll(".faq-item").forEach(item => {
   item.addEventListener("click", function () {
     let answer = item.querySelector(".faq-answer");
     let isActive = answer.style.display === "block";
 
-    // Close all other open answers
     document.querySelectorAll(".faq-answer").forEach(a => {
       gsap.to(a, { height: 0, duration: 0.3, display: 'none' });
     });
 
-    // Toggle current answer
     if (!isActive) {
       gsap.to(answer, { height: "auto", duration: 0.3, display: 'block' });
     }
